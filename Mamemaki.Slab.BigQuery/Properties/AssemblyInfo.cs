feb3c297004359b3f442c4c3f48bdf8feb2f8e3b
tsu1980 <tsu1980@gmail.com>
2015-08-29 19:06:46 +0900

    Reform AssemblyInfo.cs

--- Mamemaki.Slab.BigQuery/Properties/AssemblyInfo.cs
+++ Mamemaki.Slab.BigQuery/Properties/AssemblyInfo.cs
@@ -2,35 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
-// アセンブリに関連付けられている情報を変更するには、
-// これらの属性値を変更してください。
 [assembly: AssemblyTitle("Mamemaki.Slab.BigQuery")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SLAB(Semantic Logging Application Block) sink for Google BigQuery")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mamemaki")]
 [assembly: AssemblyProduct("Mamemaki.Slab.BigQuery")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Mamemaki 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
-// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
-// その型の ComVisible 属性を true に設定してください。
 [assembly: ComVisible(false)]
-
-// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
 [assembly: Guid("b7ce779e-e002-4fb8-be7b-87f6f8df0d9c")]
-
-// アセンブリのバージョン情報は次の 4 つの値で構成されています:
-//
-//      メジャー バージョン
-//      マイナー バージョン
-//      ビルド番号
-//      Revision
-//
-// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
-// 既定値にすることができます:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0")]
